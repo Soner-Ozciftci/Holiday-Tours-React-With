7258e0b9401127ee81f9be6d22173e52a391118a
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-23 22:38:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React , {useState,useEffect} from 'react';
+import './App.css';
+import Loading from './Loading';
+import Tours from './Tours';
+
+const url = 'https://course-api.com/react-tours-project';
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
